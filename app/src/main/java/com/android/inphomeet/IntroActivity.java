85ea849b1bcbfc1896ecfe032efed6d2ffcbcefc
Krishna Vishwakarma <unknown0871916@gmail.com>
2020-08-31 16:10:19 +0530

    Completed IntroActivity

--- app/src/main/java/com/android/inphomeet/IntroActivity.java
+++ app/src/main/java/com/android/inphomeet/IntroActivity.java
@@ -1,9 +1,18 @@
 package com.android.inphomeet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +20,43 @@ import java.util.List;
 public class IntroActivity extends AppCompatActivity {
 
     private OnboardingAdapter onboardingAdapter;
+    private LinearLayout layoutOnboardingIndicators;
+    private MaterialButton buttonOnboardingAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+        layoutOnboardingIndicators = findViewById(R.id.layoutOnboadingIndicators);
         setupOnboardingitems();
 
-        ViewPager2 onboardingViewPager = findViewById(R.id.onboardingViewPager);
+        final ViewPager2 onboardingViewPager = findViewById(R.id.onboardingViewPager);
         onboardingViewPager.setAdapter(onboardingAdapter);
+
+        buttonOnboardingAction =findViewById(R.id.buttonOnboardingAction);
+
+        setupOnboardingIndicators();
+        setCurrentOnboardingIndicator(0);
+        onboardingViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentOnboardingIndicator(position);
+            }
+        });
+
+        buttonOnboardingAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(onboardingViewPager.getCurrentItem() + 1 < onboardingAdapter.getItemCount()){
+                    onboardingViewPager.setCurrentItem(onboardingViewPager.getCurrentItem() + 1);
+                }else {
+                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                    finish();
+                }
+            }
+        });
     }
     private void setupOnboardingitems(){
         List<Onboardingitem> onboardingitems =new ArrayList<>();
@@ -51,6 +87,50 @@ public class IntroActivity extends AppCompatActivity {
         onboardingitems.add(itemcloudbased);
 
         onboardingAdapter =new OnboardingAdapter(onboardingitems);
+
+    }
+
+    private void setupOnboardingIndicators(){
+        ImageView[] indicators = new ImageView[onboardingAdapter.getItemCount()];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(8,0,8,0);
+        for(int i = 0; i<indicators.length; i++){
+            indicators[i]= new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.onboarding_indicator_active
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            layoutOnboardingIndicators.addView(indicators[i]);
+        }
+    }
+    @SuppressLint("SetTextI18n")
+    private void setCurrentOnboardingIndicator(int index){
+        int childCount = layoutOnboardingIndicators.getChildCount();
+        for(int i=0; i < childCount;i++){
+            ImageView imageView = (ImageView) layoutOnboardingIndicators.getChildAt(i);
+            if(i==index){
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_active)
+                );
+            } else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.onboarding_indicator_inactive)
+                );
+            }
+        }
+        if(index == onboardingAdapter.getItemCount() - 1){
+            buttonOnboardingAction.setText("Start");
+        }else if(index == onboardingAdapter.getItemCount() -2) {
+            buttonOnboardingAction.setText("Go");
+        }
+        else if(index == onboardingAdapter.getItemCount() -3) {
+            buttonOnboardingAction.setText("Set");
+        }else {
+            buttonOnboardingAction.setText("Get");
+        }
     }
 }
 ////Free and Powerfull  Free platform for all without any ads or limits//
