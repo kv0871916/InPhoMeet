c2266e0e4c181e813a785574d8399c21ee012e19
Krishna Vishwakarma <unknown0871916@gmail.com>
2020-09-03 04:08:47 +0530

    added Google sign in with MainLoginActivity.java

---
+++ app/src/main/java/com/android/inphomeet/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.android.inphomeet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+    private Runnable runnable =new Runnable() {
+        @Override
+        public void run() {
+            if(!isFinishing()){
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, 2000);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        handler.removeCallbacks(runnable);
+    }
+}
